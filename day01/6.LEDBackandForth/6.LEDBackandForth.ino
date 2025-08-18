c199905d6095c04fda5fcceccf2654b52c38c5fc
Shrinidhi <shrinidhihegde949@gmail.com>
2025-08-18 21:14:07 +0530

    Delay function

---
+++ day01/6.LEDBackandForth/6.LEDBackandForth.ino
@@ -0,0 +1,23 @@
+int leds[] = {A2,A3,A4,A5}; 
+int numLEDs = 4;
+int delayTime = 200; 
+int direction = 1; d
+int currentLED = 0;
+
+void setup() {
+  for (int i = 0; i < numLEDs; i++) {
+    pinMode(leds[i], OUTPUT);
+  }
+}
+
+void loop() {
+    for (int i = 0; i < numLEDs; i++) {
+        digitalWrite(leds[i], LOW);
+    }
+
+    digitalWrite(leds[currentLED], HIGH);
+    currentLED += direction;
+    if (currentLED >= numLEDs - 1 || currentLED <= 0) {
+        direction *= -1; 
+    }
+}
